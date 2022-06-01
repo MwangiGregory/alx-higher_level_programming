fe0c49f3504d23411457f248a95907c6b5f458bd
MwangiGregory <gmwaburi@gmail.com>
2022-06-01 17:06:03 +0000

    Make pow return appropritate types

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,12 +2,12 @@
 
 
 def pow(a, b):
-    a = float(a)
     if (b == 0):
         return 1
     elif (b == 1):
         return a
     elif (b < 0):
+        a = float(a)
         return ((1 / a) * pow(a, b + 1))
     else:
-        return a * pow(a, b - 1)
+        return (a * pow(a, b - 1))
