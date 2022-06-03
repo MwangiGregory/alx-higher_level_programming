8b6aac96c4a337639ea8907606fc5b4768ec29ef
MwangiGregory <gmwaburi@gmail.com>
2022-06-03 09:39:42 +0000

    Use python exponential operator

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
-
 def pow(a, b):
-    if (b == 0):
-        return 1
-    elif (b == 1):
-        return a
-    elif (b < 0):
-        a = float(a)
-        return ((1 / a) * pow(a, b + 1))
-    else:
-        return (a * pow(a, b - 1))
+    return (a ** b)
