1b500e983c74905c4179e82ecb5f0b7c12c15b1e
MwangiGregory <gmwaburi@gmail.com>
2022-06-06 21:36:04 +0000

    Check if passed list parameter is valid

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,13 @@
 def max_integer(my_list=[]):
     mx = 0
     my_list_len = len(my_list)
-    if my_list_len == 0:
-        return None
+    if my_list:
+        if my_list_len == 0:
+            return None
 
-    for i in range(0, my_list_len):
-        if my_list[i] > mx:
-            mx = my_list[i]
+        for i in range(0, my_list_len):
+            if my_list[i] > mx:
+                mx = my_list[i]
+    else:
+        return None
     return mx
