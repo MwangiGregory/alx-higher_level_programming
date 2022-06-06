de9b47b57baa4752419fd3a43f0791b2a35c37bd
MwangiGregory <gmwaburi@gmail.com>
2022-06-06 19:09:25 +0000

    Initial commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for lst in matrix:
+        for i in range(len(lst)):
+            print("{:d}".format(lst[i]), end='')
+            if (i != (len(lst) - 1)):
+                print(" ", end='')
+        print()
