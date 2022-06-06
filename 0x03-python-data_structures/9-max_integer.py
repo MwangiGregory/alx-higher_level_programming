fe3ca723581a2a4b8b76a81e3e066fc6da7fe8cc
MwangiGregory <gmwaburi@gmail.com>
2022-06-06 21:23:54 +0000

    Initial commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    mx = 0
+    my_list_len = len(my_list)
+    if my_list_len == 0:
+        return None
+
+    for i in range(0, my_list_len):
+        if my_list[i] > mx:
+            mx = my_list[i]
+    return mx
