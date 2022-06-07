e8018238b979ea729938033b95f3a3e1ce7d0732
MwangiGregory <gmwaburi@gmail.com>
2022-06-07 16:55:50 +0000

    Define function comp_list
    This function is used to compare two lists to
    determine if they are equal.
    I also rectified betty linter errors

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -27,13 +27,45 @@ listint_t *reverse_list(listint_t **head)
 	return (*head);
 }
 
+/**
+ * comp_list - compares two singly linked lists
+ * @l1: pointer to a listint_t list
+ * @l2: pointer to a listint_t list
+ * Return: 0 if list are not equal, 1 if they are equal
+ */
+int comp_list(listint_t *l1, listint_t *l2)
+{
+	int status = 0;
+
+	while (l1 != NULL)
+	{
+		if (l1->n == l2->n)
+		{
+			status = 1;
+		}
+		else
+		{
+			status = 0;
+			break;
+		}
+		l1 = l1->next;
+		l2 = l2->next;
+	}
+	return (status);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a
+ * palindrome
+ * @head: pointer to a pointer to a singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
 	listint_t *fast_pointer = *head;
 	listint_t *slow_pointer = *head;
 	listint_t *head2 = NULL;
 	listint_t *temp = NULL;
-	int i = 0, j = 0;
 
 	while (1)
 	{
@@ -41,16 +73,12 @@ int is_palindrome(listint_t **head)
 			break;
 		else if (fast_pointer->next == NULL)
 			break;
-		else
-		{
-			temp = slow_pointer;
-			fast_pointer = fast_pointer->next->next;
-			slow_pointer = slow_pointer->next;
-			i += 2;
-			j++;
-		}
+		temp = slow_pointer;
+		fast_pointer = fast_pointer->next->next;
+		slow_pointer = slow_pointer->next;
 	}
 	temp->next = NULL;
 	head2 = reverse_list(&slow_pointer);
-	return (i);
+
+	return (comp_list(*head, head2));
 }
