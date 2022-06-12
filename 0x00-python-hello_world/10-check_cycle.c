062207a418a232de1acfd439f7f25c3a8a8b9c2d
MwangiGregory <gmwaburi@gmail.com>
2022-06-12 20:23:06 +0000

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,21 +7,16 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp_head = NULL;
-	listint_t *current = list;
+	listint_t *slow_pointer = list;
+	listint_t *fast_pointer = list;
 
-	while (current)
+	while (slow_pointer && fast_pointer && fast_pointer->next)
 	{
-		temp_head = list;
-		current = current->next;
-		while (current != temp_head)
-		{
-			if (!current)
-				return (0);
-			else if (current->next == temp_head)
-				return (1);
-			temp_head = temp_head->next;
-		}
+		slow_pointer = slow_pointer->next;
+		fast_pointer = fast_pointer->next->next;
+
+		if (slow_pointer == fast_pointer)
+			return (1);
 	}
 	return (0);
 }
