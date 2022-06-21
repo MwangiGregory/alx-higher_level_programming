0787b531166d934a9895abcfd34fabd0f8eeda14
MwangiGregory <gmwaburi@gmail.com>
2022-06-21 22:04:36 +0000

    Define constructor

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Definition for class Square"""
+
+
+class Square:
+    def __init__(self, size):
+        """__init__
+
+        Initialize the object of Square
+
+        Args:
+            size (int): The size of the square
+
+        """
+        self.__size = size
