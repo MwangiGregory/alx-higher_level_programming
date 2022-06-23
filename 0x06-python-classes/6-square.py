56ca950cda68f11e4e376426c86f231cc23b880e
MwangiGregory <gmwaburi@gmail.com>
2022-06-23 00:56:54 +0000

    Raise an exception if one of coordinates is negative

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,6 +44,8 @@ class Square:
                 if type(value[0]) == int and type(value[1]) == int:
                     if value[0] >= 0 and value[1] >= 0:
                         self.__position = value
+                    else:
+                        raise TypeError
                 else:
                     raise TypeError
             else:
