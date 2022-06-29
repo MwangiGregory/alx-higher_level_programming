2b3c6958b2a8ae94d9604eba5d63e244067dfbef
MwangiGregory <gmwaburi@gmail.com>
2022-06-29 20:14:26 +0000

    Use function isinstance to validate int type

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
     def width(self, value):
         """Validate then mutate width value"""
 
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >=0")
@@ -38,7 +38,7 @@ class Rectangle:
     def height(self, value):
         """Validate and mutate  height value"""
 
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
