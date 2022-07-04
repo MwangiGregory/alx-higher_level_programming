57bee831f331867abd5f43f7ef884a5ce310ae27
MwangiGregory <gmwaburi@gmail.com>
2022-07-04 21:31:23 +0000

    Define function area()

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an empty class"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def area(self):
+        """Unimplimented area method"""
+        raise Exception('area() is not implemented')
