23afa92ae2cffd9c63db701aa2cb79a96731aeed
MwangiGregory <gmwaburi@gmail.com>
2022-07-04 21:50:21 +0000

    Define class Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines an empty class"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def area(self):
+        """Unimplimented area method"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validate value"""
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+class Rectangle(BaseGeometry):
+    """Class that defines a rectangle"""
+
+    def __init__(self, width, height):
+        """Initializer"""
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
