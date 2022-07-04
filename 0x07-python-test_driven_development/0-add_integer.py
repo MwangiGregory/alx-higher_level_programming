dc6399d312cde573a3ef28ca5e2351a1669dd0f5
MwangiGregory <gmwaburi@gmail.com>
2022-07-04 07:56:38 +0000

    Clean up docstring

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-""" Definition of function add_integer
-
-The function performs addition operation between
-two values. The values can either be floats or integers.
-"""
+""" Definition of function add_integer"""
 
 def add_integer(a, b=98):
-    """Adds two numbers
-
-    Args:
-        a (:obj:`int`, `float`): Must be an integer or float value
-        b (:obj:`int`, `float`, optional): Must be an integer or float value.
-
-    Returns:
-        int: Sum of a and b
-    """
+    """Adds two numbers"""
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
         raise TypeError('b must be an integer')
-    if type(a) or type(b) is float:
+    if type(a) is float:
         a = int(a)
+    if type(b) is float:
         b = int(b)
     return a + b
