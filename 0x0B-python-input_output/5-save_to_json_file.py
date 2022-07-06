3f586bc4b8b6e37b0d19b920096517474d9fb3d4
MwangiGregory <gmwaburi@gmail.com>
2022-07-06 19:54:09 +0000

    Add definition for function save_to_json_file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines function save_to_json_file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an Onject to a text file using a JSON
+    representation
+    """
+    with open(filename, 'w', encoding="utf-8") as a_file:
+        json.dump(my_obj, a_file)
