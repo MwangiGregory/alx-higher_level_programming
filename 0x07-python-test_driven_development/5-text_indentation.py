addb421f1904b73124c017148aa1ce417432a769
MwangiGregory <gmwaburi@gmail.com>
2022-07-07 21:29:29 +0000

    Handle case when there are sequential space chars

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,17 +11,24 @@ def text_indentation(text):
         raise TypeError('text must be a string')
 
     text_length = len(text)
-    for i in range(text_length):
+    i = 0
+    while i < text_length:
         if i == 0 or i == (text_length - 1):
             if text[i] == " ":
+                i += 1
                 continue
             else:
                 print(text[i], end="")
+                i += 1
                 continue
-        if text[i] == " " and text[i - 1] in [".", "?", ":"]:
-            continue
-        else:
-            print(text[i], end="")
 
         if text[i] in [".", "?", ":"]:
             print("\n")
+            i += 1
+            while i < text_length and text[i] == " ":
+                i += 1
+            continue
+        else:
+            print(text[i], end="")
+            i += 1
+            continue
