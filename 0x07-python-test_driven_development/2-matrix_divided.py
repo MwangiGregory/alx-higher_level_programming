e1dd65ca7317bb0ab0268e12de29686e3d91f141
MwangiGregory <gmwaburi@gmail.com>
2022-07-07 10:54:35 +0000

    Add definition for function matrix_divided

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,38 @@
+#!/usr/bin/pyhton3
+"""This module defines function matrix_divided"""
+
+
+def matrix_divided(matrix, div):
+    """Divides all the elements of a matrix"""
+
+    err_mes = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(matrix, list):
+        raise TypeError(err_mes)
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(err_mes)
+        for item in row:
+            if type(item) not in [int, float]:
+                raise TypeError(err_mes)
+
+    row_sizes = [len(row) for row in matrix]
+    first_element = row_sizes[0]
+    for i in row_sizes:
+        if i != first_element:
+            raise TypeError('Each row of the matrix must have the same size')
+
+    if type(div) not in [int, float]:
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by Zero')
+
+    new_matrix = []
+    for row in matrix:
+        temp_list = []
+        for item in row:
+            result = round(item / div, 2)
+            temp_list.append(result)
+
+        new_matrix.append(temp_list[:])
+
+    return (new_matrix)
