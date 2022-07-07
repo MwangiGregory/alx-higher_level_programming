daebdf72b6e7ba8148117d3c072cc8e6c2d86489
MwangiGregory <gmwaburi@gmail.com>
2022-07-07 18:23:35 +0000

    Remove unecessary code

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,9 +12,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
 
-    if first_name == "":
-        print(f"My name is {last_name}")
-    elif last_name == "":
-        print(f"My name is {first_name}")
-    else:
-        print(f"My name is {first_name} {last_name}")
+    print(f"My name is {first_name} {last_name}")
