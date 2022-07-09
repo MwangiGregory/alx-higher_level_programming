2359cc23630643aacf8e16722554af105834b2cd
MwangiGregory <gmwaburi@gmail.com>
2022-07-09 19:41:55 +0000

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-
+"""This module defines class MyList"""
 
 class MyList(list):
+    """This class inherits from list and add its own
+    functionality"""
+
     def print_sorted(self):
+        """Prints the list, but sorted (ascending sort)"""
         if None in self:
             raise TypeError('List can only have integers')
         new_list = self[:]
