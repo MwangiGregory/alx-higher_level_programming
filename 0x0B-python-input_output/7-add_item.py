a5bcf9a78b2407e05a02ce209d646ae2f0394379
MwangiGregory <gmwaburi@gmail.com>
2022-07-10 17:08:17 +0000

    Change position of module doc

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 """
 This module defines a script that adds
 command line arguments to a python list, and then
 saves them to a file
 """
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 filename = "add_item.json"
 contents = []
