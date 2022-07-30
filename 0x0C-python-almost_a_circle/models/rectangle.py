301b092707f42b2406bf212d7587e4d9f5d0ca3b
MwangiGregory <gmwaburi@gmail.com>
2022-07-30 08:34:05 +0000

    Add file with definition of type rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,47 @@
+"""This module defines class Rectangle"""
+from models.base import Base
+
+class Rectangle(Base):
+    """This class defines a new type Rectangle that
+    inherits from base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializer for rectangle objects"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
