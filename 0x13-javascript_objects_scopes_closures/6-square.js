79e826b7567b4e91a42137c2ebfa5cf81ade198c
MwangiGregory <gmwaburi@gmail.com>
2022-09-07 22:20:54 +0000

    Add file 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      super.print();
+    } else {
+      for (let h = 0; h < this.height; h++) {
+        let row = '';
+        for (let w = 0; w < this.width; w++) {
+          row += c;
+        }
+        console.log(row);
+      }
+    }
+  }
+}
+
+module.exports = Square;
