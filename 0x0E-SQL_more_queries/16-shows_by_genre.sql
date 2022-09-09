c6c9526e9248afa6497166401de8a7c8814cf388
MwangiGregory <gmwaburi@gmail.com>
2022-09-09 10:13:34 +0000

    Add a statment that sorts the result set by genre name

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,4 +6,4 @@ ON tv_shows.id = tv_show_genres.show_id
 
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
