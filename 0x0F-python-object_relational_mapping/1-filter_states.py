ec34fd98603685120d8f85c54546cb5b76a8e2fb
MwangiGregory <gmwaburi@gmail.com>
2022-09-10 13:30:18 +0000

    Add backticks to table and column names in sql statement

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,12 +13,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database)
     cur = db.cursor()
-    cur.execute("""
-        SELECT id, name
-        FROM states
-        WHERE name LIKE 'N%'
-        ORDER BY id ASC
-        """)
+    cur.execute("
+        SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY `id` ASC                 ")
     rows = cur.fetchall()
     for row in rows:
         print(row)
