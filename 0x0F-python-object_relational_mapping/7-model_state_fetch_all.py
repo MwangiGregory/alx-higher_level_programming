772bcac6f4131751de7d946317a49d0f8013dd72
MwangiGregory <gmwaburi@gmail.com>
2022-09-11 23:07:44 +0000

    Remove unnecessary print statement

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,6 +14,5 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    print(session)
     for instance in session.query(State).order_by(State.id):
-        print(f"{instance.id}: {instance.name}")
+        print("{}: {}".format(instance.id, instance.name))
