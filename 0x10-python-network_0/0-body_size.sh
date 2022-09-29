b8550ce70dbae1cc3535fa4fe2992264bf1bd4cd
MwangiGregory <gmwaburi@gmail.com>
2022-09-29 21:35:13 +0000

    Add comment

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# This scipt takes in a URL, sends a request to that
+# URL, and displays the size of the body of the response.
 url="$1"
 curl -s -o /dev/null "${url}" -w %{size_download}"\n"
