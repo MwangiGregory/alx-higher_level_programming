f3b70f2e7ae11c6e9ad2779780133f11c659e1a5
MwangiGregory <gmwaburi@gmail.com>
2022-10-01 09:58:29 +0000

    Add script 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script sends a request to a url and displays the body
+of the response and handles any HTTPError"""
+
+if __name__ == "__main__":
+    import sys
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+
+    url = sys.argv[1]
+
+    try:
+        response = urlopen(url)
+    except HTTPError as e:
+        status_code = e.code
+        print(f"Error code: {status_code}")
+    else:
+        print(response.read().decode('utf-8')
