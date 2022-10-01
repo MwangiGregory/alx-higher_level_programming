b81d3f539553bdeb5b8c60f853c24fa4929d116b
MwangiGregory <gmwaburi@gmail.com>
2022-10-01 09:21:03 +0000

    Add script 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This script sends a POST request to a url with the email
+as a parameter, and displays the body of the response"""
+
+if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    from urllib.parse import urlencode
+    import sys
+
+    url, email = sys.argv[1], sys.argv[2]
+    data = {'email': email}
+    data = urlencode(values)
+    data = data.encode('ascii')
+    req = Request(url, data)
+
+    with urlopen(req) as response:
+        body = response.read()
+
+    character_set = response.headers.get_content_charset()
+    decoded_body = body.decode(character_set)
+    print(decoded_body)
