c01b432549cf245e61540fe461bcd83da43003a3
MwangiGregory <gmwaburi@gmail.com>
2022-12-09 22:25:15 +0300

    Add script 8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  if (list) {
+    const reversedList = [];
+    for (let i = 0; i <= list.length - 1; i++) {
+      reversedList.unshift(list[i]);
+    }
+    return reversedList;
+  }
+};
