577a1b9a11dca645e880c6ca8807dd512e4f67ef
MwangiGregory <gmwaburi@gmail.com>
2022-12-09 22:07:46 +0300

    Add script 7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  if (list && searchElement) {
+    for (let i = 0; i <= list.length - 1; i++) {
+      if (list[i] === searchElement) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
