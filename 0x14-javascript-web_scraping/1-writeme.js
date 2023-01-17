ed60b91c93ccb94ff8dedbabe724e91860af4312
MwangiGregory <gmwaburi@gmail.com>
2023-01-17 13:09:48 +0300

    fix formatting

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const filePath = process.argv[2];
 const text = process.argv[3];
 const fs = require('fs');
 
-fs.writeFile(filePath, text, {encoding: 'utf-8'}, (err) => {
+fs.writeFile(filePath, text, { encoding: 'utf-8' }, (err) => {
   if (err) {
     console.log(err);
   }
