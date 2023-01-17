690b48cc32355922f2275401018d4edd09cf223f
MwangiGregory <gmwaburi@gmail.com>
2023-01-17 22:13:39 +0300

    update script

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,21 +2,21 @@
 
 const request = require('request');
 const url = process.argv[2];
-const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+const characterId = 18;
 
 request({ url: url, encoding: 'utf-8' }, (err, res, body) => {
   if (err) console.log(err);
-
-  const filmDetails = JSON.parse(body).results;
   let count = 0;
+  const results = JSON.parse(body).results;
 
-  for (let i = 0; i < filmDetails.length; i++) {
-    const film = filmDetails[i];
-    const characters = film.characters;
+  for (let i = 0; i < results.length; i++) {
+    const filmDetails = results[i];
+    const characters = filmDetails.characters;
 
     for (let j = 0; j < characters.length; j++) {
       const url = characters[j];
-      if (url === characterUrl) {
+
+      if (url.endsWith(characterId.toString().concat('/'))) {
         count++;
       }
     }
