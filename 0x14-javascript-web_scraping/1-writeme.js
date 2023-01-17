1f7d9f33cac32355b63ee8a8e8675c7d2515bb8e
MwangiGregory <gmwaburi@gmail.com>
2023-01-17 13:08:55 +0300

    Add script 1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const filePath = process.argv[2];
+const text = process.argv[3];
+const fs = require('fs');
+
+fs.writeFile(filePath, text, {encoding: 'utf-8'}, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
