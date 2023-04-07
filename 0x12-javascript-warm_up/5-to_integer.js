26a37e3aeddb6c526c7354870551862c1fa4f0b3
MwangiGregory <gmwaburi@gmail.com>
2023-04-07 21:49:43 +0300

    Make all variables use camel case

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const number_arg = parseInt(args[2]);
+const numberArg = parseInt(args[2]);
 
-if (!number_arg) {
+if (!numberArg) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${number_arg}`);
+  console.log(`My number: ${numberArg}`);
 }
