d2cc92df2e8e6df95da68872a3275e680470db89
MwangiGregory <gmwaburi@gmail.com>
2023-04-09 23:50:41 +0300

    Add script

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 || h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let row = 0; row < this.height; row++) {
+      let line = '';
+      for (let column = 0; column < this.width; column++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+};
