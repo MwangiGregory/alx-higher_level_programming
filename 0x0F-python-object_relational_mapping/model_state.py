e73bdc2279e75235f96b83646061b2ff5281e368
MwangiGregory <gmwaburi@gmail.com>
2023-04-22 15:15:39 +0300

    Add script

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This script contains the class definition of a State"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class that is mapped to the states table in the hbtn_0e_6_usa database"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
