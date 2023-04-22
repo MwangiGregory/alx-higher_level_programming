6ea721a030121b59ed02c1688ea7d371eb8b7b63
MwangiGregory <gmwaburi@gmail.com>
2023-04-22 12:07:44 +0300

    Add Script

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This script takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username, password, db_name, state_name = sys.argv[1:]
+    db = MySQLdb.connect(
+        host='localhost', port=3306,
+        user=username, passwd=password, database=db_name
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states \
+         WHERE states.name LIKE %s \
+         ORDER BY states.id ASC",
+        (state_name,))
+
+    for state in cur.fetchall():
+        print(state)
