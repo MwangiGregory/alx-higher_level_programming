1c0934d182468067e2df6434345bf64c6e4c9941
MwangiGregory <gmwaburi@gmail.com>
2023-04-22 14:51:37 +0300

    Add script

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This script takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username, password, db_name, state_name = sys.argv[1:]
+    db = MySQLdb.connect(
+        host='localhost', port=3306,
+        user=username, passwd=password, database=db_name
+    )
+    cur = db.cursor()
+    cur.execute(
+        """
+        SELECT cities.name FROM cities
+        WHERE cities.state_id = (
+        SELECT states.id
+        FROM states
+        WHERE states.name = %s)""", (state_name,)
+    )
+
+    cities = cur.fetchall()
+    for index, city in enumerate(cities):
+        print(city[0], end='')
+        if index != len(cities) - 1:
+            print(', ', end='')
+    print()
