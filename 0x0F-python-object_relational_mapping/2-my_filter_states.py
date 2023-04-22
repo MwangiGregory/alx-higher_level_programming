577e9d509400004fa6dcb6a1e00dd0532fc34ee6
MwangiGregory <gmwaburi@gmail.com>
2023-04-22 11:48:16 +0300

    Use LIKE operator

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE states.name = '{}' ORDER BY states.id ASC"
+        "SELECT * FROM states \
+         WHERE states.name LIKE '{}' \
+         ORDER BY states.id ASC"
         .format(state_name))
 
     for state in cur.fetchall():
