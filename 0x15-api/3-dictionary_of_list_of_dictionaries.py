bc976b19112f91f345d4929f36c7f86862250426
MwangiGregory <gmwaburi@gmail.com>
2023-04-25 02:06:18 +0300

    Minor bug fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,17 +8,18 @@ if __name__ == "__main__":
     users = requests.get('https://jsonplaceholder.typicode.com/users').json()
     temp = {}
     for user in users:
-        temp[user.get('id')] = []
+        userId = user.get('id')
         tasks = requests.get(
-            f'https://jsonplaceholder.typicode.com/todos? \
-                userId={user.get("id")}').json()
-        # print(type(tasks))
+            f'https://jsonplaceholder.typicode.com/todos?userId={userId}')
+        tasks = tasks.json()
+        temp_list = []
         for task in tasks:
             temp_dict = {}
             temp_dict["username"] = user.get("username")
             temp_dict["completed"] = task.get("completed")
             temp_dict["task"] = task.get("title")
-            temp[user.get('id')].append(temp_dict)
+            temp_list.append(temp_dict)
+        temp[userId] = temp_list
 
     with open('todo_all_employees.json', 'w', encoding='utf8') as f:
         json.dump(temp, f)
