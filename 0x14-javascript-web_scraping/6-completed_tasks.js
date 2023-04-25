1ad6ae7abb913101b4da70729b70884fe89f02ed
MwangiGregory <gmwaburi@gmail.com>
2023-04-26 01:07:11 +0300

    Add solution for task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,27 +1,24 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
 
-request({ uri: url, encoding: 'utf-8' }, (err, res, body) => {
-  if (err) console.log(err);
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
 
   const todos = JSON.parse(body);
-  const temp = {};
-  for (let i = 0; i < todos.length; i++) {
-    const todo = todos[i];
-    const userId = todo.userId;
-    const completed = todo.completed;
 
-    if (!(userId.toString() in temp)) {
-      temp[userId.toString()] = 0;
+  const completedTasksByUserId = {};
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      const userId = todo.userId;
+      const completedTasks = completedTasksByUserId[userId] || 0;
+      completedTasksByUserId[userId] = completedTasks + 1;
     }
-    if (completed) {
-      temp[userId.toString()] = temp[userId.toString()] + 1;
-    }
-  }
-  for (const key in temp) {
-    if (temp[key] === 0) delete temp[key];
-  }
-  console.log(temp);
+  });
+
+  console.log(completedTasksByUserId);
 });
