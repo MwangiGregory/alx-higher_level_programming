2587415c51b1a2483b66054004ecdfdd69c4fea5
MwangiGregory <gmwaburi@gmail.com>
2023-04-26 00:35:03 +0300

    Add solution for task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
-
 const request = require('request');
 const fs = require('fs');
+
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request({ url: url, encoding: 'utf-8' }, (err, res, body) => {
-  if (err) console.log(err);
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-  fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
-    if (err) console.log(err);
-  });
+  fs.writeFileSync(filePath, body, 'utf-8');
 });
