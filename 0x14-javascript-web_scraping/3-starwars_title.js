1fd1ea9042d7b9e8602e66d8d18b076740e1b11e
MwangiGregory <gmwaburi@gmail.com>
2023-04-26 00:06:28 +0300

    Add solution for task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
-
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request({ url: url, encoding: 'utf-8' }, (err, res, body) => {
-  if (err) console.log(err);
-  console.log(JSON.parse(body).title);
+const movieId = process.argv[2]; // get the movie ID from the command-line arguments
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
 });
