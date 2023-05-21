3abcff96e1213b4d2ef467289d9126654a363f44
MwangiGregory <gmwaburi@gmail.com>
2023-05-21 11:33:00 +0300

    Add solution for task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Fetches https://alx-intranet.hbtn.io/status
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        data = res.read()
+        print("Body response:")
+        print(f"    - type: {type(data)}")
+        print(f"    - content: {data}")
+        print(f"    - utf8 content: {data.decode('utf-8')}")
