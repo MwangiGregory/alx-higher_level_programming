b8ba24ba9f8653966e77438c27a8128e186a7a84
MwangiGregory <gmwaburi@gmail.com>
2023-05-21 11:38:35 +0300

    Minor fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
         data = res.read()
         print("Body response:")
-        print(f"    - type: {type(data)}")
-        print(f"    - content: {data}")
-        print(f"    - utf8 content: {data.decode('utf-8')}")
+        print(f"- type: {type(data)}")
+        print(f"- content: {data}")
+        print(f"- utf8 content: {data.decode('utf-8')}")
